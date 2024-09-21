ae10b4341e4ef79bfc318ab61468aa236a332dfb
Armaan Priyadarshan <armaan_priyadarshan@outlook.com>
2024-09-21 06:08:46 -0400

    were completely cooked

--- seeshell/app/index.tsx
+++ seeshell/app/index.tsx
@@ -1,15 +1,13 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
-export default function Index() {
+const Index = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View>
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
+
+export default Index;
+
+const styles = StyleSheet.create({});
