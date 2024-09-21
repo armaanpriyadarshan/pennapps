0301286060c6762126a0c51f18ad44e096109c87
Armaan Priyadarshan <armaan_priyadarshan@outlook.com>
2024-09-21 10:07:06 -0400

    cooked

--- seeshell/app/map.tsx
+++ seeshell/app/map.tsx
@@ -1,11 +1,23 @@
-import { View, Text } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import mapStyle from './mapStyle.json';
 
 const Map = () => {
   return (
-    <View>
-      <Text>Map</Text>
+    <View style={styles.container}>
+      <MapView style={styles.map} provider={PROVIDER_GOOGLE} customMapStyle={mapStyle} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
 
 export default Map;
